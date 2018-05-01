6346055b77210ad026d059862f959f49c58a5f0a
hallowf <alexandre.cortegaca@gmail.com>
2018-05-01 15:58:14 +0100

    Added script to encrypt with RSA or AES

---
+++ encrypt_file_AES.py
@@ -0,0 +1,13 @@
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+
+file = open("requirements_test.txt", "r")
+data_str = file.read()
+data = data_str.encode("latin-1")
+
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_EAX)
+ciphertext, tag = cipher.encrypt_and_digest(data)
+
+file_out = open("encrypted.bin", "wb")
+[ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
